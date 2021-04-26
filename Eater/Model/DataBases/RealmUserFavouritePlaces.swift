0febc485ad772094f584a8918f912b4d61c4b698
Dmytro Pupena <d.pupena@pnn.in.ua>
2021-04-26 12:01:46 +0300

    added pods and created text realm file

---
+++ Eater/Model/DataBases/RealmUserFavouritePlaces.swift
@@ -0,0 +1,14 @@
+//
+//  RealmUserFavouritePlaces.swift
+//  Eater
+//
+//  Created by Dmitriy Pupena on 26.04.2021.
+//
+
+import Foundation
+import Realm
+import RealmSwift
+
+class RealmUserFavouritePlaces: Object {
+    @objc dynamic var key: String!
+}
